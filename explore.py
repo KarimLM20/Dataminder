57939080ac7c15f5b7c0fc7396cf5722ca009720
KarimLM20 <kbenhafedh@gmail.com>
2025-06-09 15:03:34 +0200

    Your commit message

--- explore.py
+++ explore.py
@@ -7,3 +7,15 @@ import plotly.express as px
 
 name_dataset = pd.read_csv('titlerating.csv')
 # Chargement des datasets disponibles
+st.title("Exploration des données")
+st.write("Sélectionnez un dataset pour l'explorer :")   
+
+df = st.selectbox("Quel dataset veux-tu utiliser ?",name_dataset)
+
+set = sns.load_dataset(df)
+
+dfchoisi= set.columns.to_list()
+
+st.dataframe(set)
+st.write("Voici les colonnes disponibles dans le dataset :")
+st.write(dfchoisi)
